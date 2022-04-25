e86c3e1f686844b2df8ec360813604130e2a1dba
devlights <gsf.zero1@devlights.onmicrosoft.com>
2022-04-25 07:47:35 +0000

    Update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/devlights/gotmp
 
 go 1.18
+
+require github.com/devlights/gomy v0.4.1
--- main.go
+++ main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"os"
+	"time"
+
+	"github.com/devlights/gomy/chans"
+	"github.com/devlights/gomy/output"
 )
 
 const (
@@ -17,5 +22,27 @@ func run() int {
 	// ------------------------------
 	// ↓↓↓ PUT SAMPLE CODE BELOW ↓↓↓
 	// ------------------------------
+	var (
+		rootCtx          = context.Background()
+		mainCtx, mainCxl = context.WithCancel(rootCtx)
+	)
+	defer mainCxl()
+
+	var (
+		procCtx, procCxl = context.WithTimeout(mainCtx, 1*time.Second)
+		done             = procCtx.Done()
+		gen              = chans.Generator(done, 1, 2, 3, 4, 5)
+		proc             = chans.Map(done, gen, func(v int) int { return v * 2 })
+		out              = chans.Buffer(done, proc, 3)
+	)
+	defer procCxl()
+
+	for v := range out {
+		output.Stdoutl("[len]", len(v))
+		for _, x := range v {
+			output.Stdoutf("[===>]", "\t%v\n", x.After)
+		}
+	}
+
 	return SUCCESS
 }
